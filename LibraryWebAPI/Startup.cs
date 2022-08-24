fc08c27caa445a7ac401078f78014b7e2ebb15e2
Rosalice Nogueira <72668915+rosaliceng@users.noreply.github.com>
2022-08-24 14:26:13 -0300

    Add files via upload

--- LibraryWebAPI/Startup.cs
+++ LibraryWebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -29,13 +30,15 @@ namespace LibraryWebAPI
             services.AddDbContext<LibraryContext>(
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
                 );
-            services.AddScoped<IRepository, Repository>();
 
             services.AddControllers()
                     .AddNewtonsoftJson(
                         opt => opt.SerializerSettings.ReferenceLoopHandling =
                         Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-                        
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
